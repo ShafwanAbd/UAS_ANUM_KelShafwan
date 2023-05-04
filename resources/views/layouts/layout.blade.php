d02141b88ff2047db5976373cd25fd2b6c5229e7
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2023-05-04 21:49:32 +0700

    p

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -14,11 +14,6 @@
 </head>
 
 <body> 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-        </div>
-    </nav>
     @yield('content')
 </body>
 </html>
